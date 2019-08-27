9e12acf33f514c696824be6f8cd7e7b011e699bf
cj <1135528371@qq.com>
2019-08-27 19:04:07 +0800

    提交

--- student-manage/src/App.vue
+++ student-manage/src/App.vue
@@ -1,20 +1,34 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="nav">
+      <router-link to="/login">登录与注册</router-link>
+      <router-link to="/">主页</router-link>
+       <router-link to='/keyCharts'>charts</router-link>
     </div>
-    <router-view/>
+
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
+* {
+  margin: 0;
+  padding: 0;
 }
 #nav {
   padding: 30px;
@@ -25,5 +39,16 @@
       color: #42b983;
     }
   }
+}
+.nav {
+  position: fixed;
+  right: 0;
+  top:0;
+  width: 20%;
+  height: 100%;
+  background: salmon;
+}
+.nav a {
+  display: block;
 }
 </style>
--- student-manage/src/views/Home.vue
+++ student-manage/src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+   <div class="homebox">
+     使用合理的布局实现PC和移动端兼容，请勿使用第三方UI框架，可单独使用第三方日历插件，要求精简高效，样式可自行设计，简洁美观大方即可，不要花里胡哨
+   </div>
   </div>
 </template>
 
@@ -16,3 +17,15 @@ export default Vue.extend({
   },
 });
 </script>
+<style lang="scss" scoped>
+.home{
+  width: 80%; 
+}
+.homebox{
+  width: 576px;
+  height: 184px;
+  margin:0 auto;
+  text-align: center;
+  padding-top: 150px; 
+}
+</style>
